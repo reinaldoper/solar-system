e51d545be8c7890dfd253b04fe07276cd78e7710
Reinaldo <reinaldoper83@gmail.com>
2022-07-20 17:33:14 -0300

    requisito 3

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Title extends React.Component {
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>
+        { headline }
+      </h2>
+    );
+  }
+}
+
+export default Title;
